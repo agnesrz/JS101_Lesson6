da6de9c518d58683c0968ebc607e7a142f397f0b
Agnes Rzepecki <aga.r909@gmail.com>
2020-09-04 01:27:37 +0000

    added bonus features; program has some bugs I still need to work through

--- twenty-one_walkthrough.js
+++ twenty-one_walkthrough.js
@@ -1,4 +1,22 @@
+//// note to self - SOME BUGS I NEED TO FIX; sometimes there is a premature 'you've busted' other times it skips asking me to stay to go to dealer's turn
+
 const readline = require('readline-sync');
+const fullDeck = {diamonds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'],
+                  hearts: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'],
+                  spades: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'],
+                  clubs: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']};
+
+
+function initializeDeck(fullDeck) {
+  let deck = [];
+  for (let key in fullDeck) {
+    for (let index = 0; index < fullDeck[key].length; index += 1) {
+      deck.push([key[0], fullDeck[key][index]]);
+    }
+  }
+
+  return deck;
+}
 
 function total(cards) {
   // cards = [['H', '3'], ['S', 'Q'], ... ]
@@ -17,14 +35,14 @@ function total(cards) {
 
   // correct for Aces
   values.filter(value => value === "Ace").forEach(_ => {
-    if (sum > 21) sum -= 10;
+    if (sum > GAME) sum -= 10;
   });
 
   return sum;
 }
 
-function busted(hand) {
-  if (total(hand) > 21) {
+function busted(score) {
+  if (score > GAME) {
     return true;
   }
   return false;
@@ -57,130 +75,172 @@ function dealCards(deck, hand, amount) {
   }
 }
 
-function displayCards(playerHand, dealerHand, numDealerCardsToDisplay = 'all') {
+function displayCards(playerHand, playerScore, dealerHand, dealerScore, numDealerCardsToDisplay = 'all') {
   let playerCards = playerHand.map(currVal => currVal[1]);
   let dealerCards = dealerHand.map(currVal => currVal[1]);
 
   if (numDealerCardsToDisplay === 'all') {
-    console.log(`Dealer's cards: ${dealerCards.join(', ')}`);
+    console.log(`Dealer's cards: ${dealerCards.join(', ')} (total score of ${dealerScore})`);
   } else {
     let hiddenCards = dealerHand.filter((_, idx) => idx > 0)
                                 .map(_ => '[Hidden]')
                                 .join(', ');
 
-    console.log(`Dealers cards: ${hiddenCards}, ${dealerCards[0]}`);
+    console.log(`Dealers cards:  ${dealerCards[0]}, ${hiddenCards}`);
   }
 
-  console.log(`Your cards:     ${playerCards.join(', ')}`);
+  console.log(`Your cards:     ${playerCards.join(', ')} (total score of ${playerScore})`);
 
 }
 
-
-function hitOrStay(hand) {
-  if (total(hand) >= 17) return 'stay';
+function hitOrStay(score) {
+  if (score >= 17) return 'stay';
 
   return 'hit';
 }
 
-function declareWinner(playerCards, dealerCards) {
-  if (busted(dealerCards) ||
-      (total(playerCards) > total(dealerCards) && !busted(playerCards))) {
-    console.log('Congratulations! You have won the game!\n');
-  } else if (busted(playerCards) ||
-             (total(playerCards) < total(dealerCards) &&
-              !busted(dealerCards))) {
-    console.log('The dealer has won the game!\n');
+function winner(playerScore, dealerScore, gameLevel) {// game level refers to whether the score is for the 'round' or the entire 'game'
+  if (gameLevel === 'game' && WINNING_POINTS > 1) {
+    return playerScore > dealerScore ? 'player' : 'dealer';
+  } else if (busted(dealerScore) || (playerScore > dealerScore && !busted(playerScore))) {
+    return 'player';
+  } else if (busted(playerScore) || (playerScore < dealerScore && !busted(dealerScore))) {
+    return 'dealer';
   } else {
-    console.log("It's a tie!\n");
+    return 'tie';
+  }
+}
+
+function declareWinner(playerScore, dealerScore, gameLevel) {// game level refers to whether the score is for the 'round' or the entire 'game'
+  switch (winner(playerScore, dealerScore, gameLevel)) {
+    case 'player': console.log(`Congratulations! You have won the ${gameLevel}!\n`);
+      break;
+    case 'dealer': console.log(`The dealer has won the ${gameLevel}!\n`);
+      break;
+    default: console.log("It's a tie!\n");
   }
 }
 
 function pressAnyKey() {
   console.log('\nPress any key to continue.');
   readline.question();
+  console.clear();
 }
 
-// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< START OF GAME
+// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< START OF GAME
+const WINNING_POINTS = 2;
+const GAME = 21;
 let keepPlaying = true;
+let playerTotalScore = 0;
+let dealerTotalScore = 0;
+let playerBusted = false; 
+let dealerBusted = false;
+let gameLevel = WINNING_POINTS > 1 ? 'round' : 'game';
 
-console.log('Welcome to 21!\n');
+console.log(`Welcome to ${GAME}!\n`);
 
 while (keepPlaying) {
-  let deck = [
-    ['D', 1], ['D', 2], ['D', 3], ['D', 4], ['D', 5], ['D', 6], ['D', 7], ['D', 8], ['D', 9], ['D', 10], ['D', 'Jack'], ['D', 'Queen'], ['D', 'King'], ['D', 'Ace'],
-    ['H', 1], ['H', 2], ['H', 3], ['H', 4], ['H', 5], ['H', 6], ['H', 7], ['H', 8], ['H', 9], ['H', 10], ['H', 'Jack'], ['H', 'Queen'], ['H', 'King'], ['H', 'Ace'],
-    ['S', 1], ['S', 2], ['S', 3], ['S', 4], ['S', 5], ['S', 6], ['S', 7], ['S', 8], ['S', 9], ['S', 10], ['S', 'Jack'], ['S', 'Queen'], ['S', 'King'], ['S', 'Ace'],
-    ['C', 1], ['C', 2], ['C', 3], ['C', 4], ['C', 5], ['C', 6], ['C', 7], ['C', 8], ['C', 9], ['C', 10], ['C', 'Jack'], ['C', 'Queen'], ['C', 'King'], ['C', 'Ace'],
-  ];
-
-  let playerCards = [];
-  let dealerCards = [];
-
-  shuffle(deck);
-
-  console.log('The deck is being shuffled. Press any key to be dealt your cards.\n');
-  readline.question();
-
-  dealCards(deck, dealerCards, 2);
-  dealCards(deck, playerCards, 2);
-
+  let currentRound = 0;
+  
   while (true) {
-    console.clear();
-    displayCards(playerCards, dealerCards, 1);
-
-    console.log("\nHit or stay?\n");
-    let answer = readline.question().toLowerCase();
-
-    while (answer !== 'stay' && answer !== 'hit') {
-      console.log("That's not a valid choice. Please try again.");
-      answer = readline.question().toLowerCase();
-    }
-
-    if (answer === 'hit') dealCards(deck, playerCards, 1);
-    if (answer === 'stay' || busted(playerCards)) break;
-  }
-
-  console.clear();
-
-  if (busted(playerCards)) {
-    console.log("You have busted!\n");
-  } else {
-    console.log("You chose to stay!");
-  }
-
-  pressAnyKey();
-
-  while (true) {
-    if (hitOrStay(dealerCards) === 'stay' ||
-        busted(dealerCards) ||
-        busted(playerCards)) {
-      break;
-    } else {
-      console.clear();
-      console.log("The dealer has chosen to hit.\n");
-      dealCards(deck, dealerCards, 1);
-      displayCards(playerCards, dealerCards, 1);
-
+    currentRound += 1;
+    let deck = initializeDeck(fullDeck);
+    let playerCards = [];
+    let dealerCards = [];
+ 
+    if (WINNING_POINTS > 1) {
+      currentRound > 1 ? console.log(`CURRENT SCORE\nYou: ${playerTotalScore}\nDealer: ${dealerTotalScore}`) :
+                         console.log(`The first player to win ${WINNING_POINTS} rounds wins.`);
       pressAnyKey();
     }
-  }
 
-  if (busted(dealerCards)) {
+    shuffle(deck);
+  
+    console.log('The deck is being shuffled. Press any key to be dealt your cards.\n');
+    readline.question();
+  
+    dealCards(deck, dealerCards, 2);
+    dealCards(deck, playerCards, 2);
+  
+    let playerScore = total(playerCards);
+    let dealerScore = total(dealerCards);
+  
+    while (true) {
+      console.clear();
+      displayCards(playerCards, playerScore, dealerCards, dealerScore, 1);
+  
+      console.log("\nHit or stay?\n");
+      let answer = readline.question().toLowerCase();
+  
+      while (answer !== 'stay' && answer !== 'hit') {
+        console.log("That's not a valid choice. Please try again.");
+        answer = readline.question().toLowerCase();
+      }
+  
+      if (answer === 'hit') {
+        dealCards(deck, playerCards, 1);
+        playerScore = total(playerCards);
+      }
+      
+      if (busted(playerScore)) playerBusted = true;
+      
+      if (answer === 'stay' || playerBusted) break;
+    }
+  
     console.clear();
-    console.log("The dealer has busted.\n");
-  } else if (!busted(playerCards)) {
-    console.clear();
-    console.log("The dealer has chosen to stay.\n");
+  
+    if (playerBusted) {
+      console.log("You have busted!");
+    } else {
+      console.log("You chose to stay!");
+    }
+  
+    while (true) {
+      if (busted(dealerScore)) dealerBusted = true;
+      
+      if (playerBusted || dealerBusted || hitOrStay(dealerScore) === 'stay') {
+        break;
+      } else {
+        pressAnyKey();
+        console.log("The dealer has chosen to hit.\n");
+        dealCards(deck, dealerCards, 1);
+        dealerScore = total(dealerCards);
+        displayCards(playerCards, playerScore, dealerCards, dealerScore, 1);
+      }
+    }
+  
+    if (dealerBusted) {
+      console.clear();
+      console.log("The dealer has busted.");
+    } else if (!playerBusted) {
+      console.clear();
+      console.log("The dealer has chosen to stay.");
+    }
+  
+    pressAnyKey();
+    declareWinner(playerScore, dealerScore, gameLevel);
+    displayCards(playerCards, playerScore, dealerCards, dealerScore);
+    
+    if (WINNING_POINTS > 1) {
+      switch (winner(playerScore, dealerScore)) {
+        case 'player': playerTotalScore += 1;
+          break;
+        case 'dealer': dealerTotalScore += 1;
+      }
+      pressAnyKey();      
+      
+      if (playerTotalScore === WINNING_POINTS || dealerTotalScore === WINNING_POINTS) {
+        declareWinner(playerTotalScore, dealerTotalScore, 'game');
+        break;
+      }      
+    } else {
+      console.log();
+      break;
+    }
+
   }
-
-  pressAnyKey();
-  console.clear();
-  declareWinner(playerCards, dealerCards);
-  displayCards(playerCards, dealerCards);
-  console.log();
-
   playAgain();
   console.clear();
 }
 
-console.log('Thanks for playing 21!');
+console.log(`Thanks for playing ${GAME}!\n`);
